1a9e099dbe8cd34128f3db4faa271d1528ab2c4f
Michael Tjandra <miciltjandra@gmail.com>
2016-11-11 23:18:12 +0700

    Add userinfo in identityservice

--- IdentityService/src/java/identityservice/UserInfo.java
+++ IdentityService/src/java/identityservice/UserInfo.java
@@ -5,13 +5,20 @@
  */
 package identityservice;
 
+import database.IdentityDB;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+import token.Token;
 
 /**
  *
@@ -58,7 +65,35 @@ public class UserInfo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        String id = request.getParameter("id");
+        JSONObject json = new JSONObject();
+        String query = "SELECT * FROM user WHERE user_id = " + id;
+        
+ 
+        IdentityDB db = new IdentityDB();
+        try {
+            ResultSet rs = db.select(query);
+            if(rs.next()) {
+            //Retrieve by column name
+                String fullname;
+                String address;
+                String postalcode;
+                String phonenumber;
+                fullname = rs.getString("fullname");
+                address = rs.getString("address");
+                postalcode = rs.getString("postalcode");
+                phonenumber = rs.getString("phonenumber");
+                json.put("fullname", fullname);
+                json.put("address", address);
+                json.put("postalcode", postalcode);
+                json.put("phonenumber", phonenumber);
+                out.write(json.toString());
+            }
+            db.closeDB();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserInfo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
