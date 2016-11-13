036ee9500fc4e9fc7da7eeec7b38b08df2bdb3fe
Michael Tjandra <miciltjandra@gmail.com>
2016-11-13 21:57:03 +0700

    Clean unused code in client servlet

--- Client/src/java/like/IncreaseLike.java
+++ Client/src/java/like/IncreaseLike.java
@@ -25,33 +25,7 @@ public class IncreaseLike extends HttpServlet {
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8081/Marketplace/Marketplace.wsdl")
     private Marketplace_Service service;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet IncreaseLike</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet IncreaseLike at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -67,9 +41,8 @@ public class IncreaseLike extends HttpServlet {
         PrintWriter out = response.getWriter();
         String user = request.getParameter("user_id");
         String product = request.getParameter("product_id");
-        String z = increaseLike(user, product);
-        out.println(z);
-        //out.println(product);
+        String result = increaseLike(user, product);
+        out.println(result);
     }
 
     /**
@@ -83,7 +56,7 @@ public class IncreaseLike extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
     }
 
     /**
